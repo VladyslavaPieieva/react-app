b2a14055becb83039c965ff548c1054b0ad67608
VladyslavaPieieva <107818375+VladyslavaPieieva@users.noreply.github.com>
2023-02-18 20:29:07 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import Weather from "./Weather";
+import "./Weather.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
